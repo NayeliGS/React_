a6594b898a1cd5b18929ad0f6b7202aad20e4a90
NayeliGS <gsnayeli8@gmail.com>
2023-09-14 23:14:37 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "./Counter"
+import EpisodeBrowser from "./EpisodeBrowser";
+
+export default function App (){
+  
+
+    return(
+        <div className="App">
+            <EpisodeBrowser  />
+           
+        </div>
+    );
+}
---
+++ src/Counter.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function Counter (){
+
+    const [count, setCount] = useState(0);
+
+    function increment () {
+        setCount(count + 1);
+        console.log(count); 0
+    }
+
+    return(
+        <div className="Counter">
+            <h1>Counter</h1>
+            <p>Count:{count}</p>
+            <button onClick={increment}>Click</button>
+        </div>
+    );
+}
+
