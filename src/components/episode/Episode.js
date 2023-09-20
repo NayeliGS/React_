29a6299eda95f8bf17e3f541714603f66c5378f7
NayeliGS <gsnayeli8@gmail.com>
2023-09-20 14:39:24 -0600

    ultimos cambios

--- src/components/episode/Episode.js
+++ src/components/episode/Episode.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Episode(props){
     return(
         <div className="Episode">
-            <h1>Episode</h1>
+            <h1>Episoode</h1>
             <p>Episode: {props.episode}</p>
             <p>Name: {props.name}</p>
             <img src={props.cover} alt={props.name} width={400}/>
