412cb73658895a0a4c1c5abca8eff3f2638640b4
NayeliGS <gsnayeli8@gmail.com>
2023-09-20 14:42:47 -0600

    add

--- src/components/episode/Episode.js
+++ src/components/episode/Episode.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Episode(props){
     return(
         <div className="Episode">
-            <h1>Episoode</h1>
+            <h1>Episooode</h1>
             <p>Episode: {props.episode}</p>
             <p>Name: {props.name}</p>
             <img src={props.cover} alt={props.name} width={400}/>
